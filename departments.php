f5287d0a55583318441e4ed6c08461553faa0964
naveensajan <79559627+naveensajan@users.noreply.github.com>
2021-03-17 00:03:17 +0530

    Update departments.php

--- departments.php
+++ departments.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>food items</title>
+    <title>departments</title>
 </head>
 <body style="background-color:black;">
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
